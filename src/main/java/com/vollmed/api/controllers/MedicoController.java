39c26fda10a6188e9469ca19663b047529ecdb85
gqroga <gqueirogat@gmail.com>
2025-07-16 20:57:14 -0300

    feat(medico): Implementa endpoint POST para cadastro de medicos

---
+++ src/main/java/com/vollmed/api/controllers/MedicoController.java
@@ -0,0 +1,25 @@
+package com.vollmed.api.controllers;
+
+import com.vollmed.api.medico.DadosCadastroMedico;
+import com.vollmed.api.medico.Medico;
+import com.vollmed.api.medico.MedicoRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("medicos")
+public class MedicoController {
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrarMedico(@RequestBody DadosCadastroMedico dados) {
+        medicoRepository.save(new Medico(dados));
+    }
+}
