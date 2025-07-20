38cd5329c24e6e56c5e766781d4380c3bb8c174f
gqroga <gqueirogat@gmail.com>
2025-07-20 13:13:00 -0300

    feat(paciente): Adiciona detalhamento do Paciente e padroniza retornos HTTP

---
+++ src/main/java/com/vollmed/api/paciente/DadosDetalhamentoPaciente.java
@@ -0,0 +1,18 @@
+package com.vollmed.api.paciente;
+
+import com.vollmed.api.endereco.DadosEndereco;
+import com.vollmed.api.endereco.Endereco;
+
+public record DadosDetalhamentoPaciente(
+
+        Long id,
+        String nome,
+        String email,
+        String cpf,
+        String telefone,
+        Endereco endereco) {
+
+    public DadosDetalhamentoPaciente (Paciente paciente) {
+        this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getCpf(), paciente.getTelefone(), paciente.getEndereco());
+    }
+}
