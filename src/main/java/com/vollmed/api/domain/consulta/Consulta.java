2861516f6dc9a514f7c51b668eaf883ceb55d0fb
gqroga <gqueirogat@gmail.com>
2025-07-29 14:16:39 -0300

    feat(consultas): Implementa base para agendamento de consultas

---
+++ src/main/java/com/vollmed/api/domain/consulta/Consulta.java
@@ -0,0 +1,31 @@
+package com.vollmed.api.domain.consulta;
+
+import com.vollmed.api.domain.medico.Medico;
+import com.vollmed.api.domain.paciente.Paciente;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Table(name = "consultas")
+@Entity(name = "Consulta")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Consulta {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "medico_id")
+    private Medico medico;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "paciente_id")
+    private Paciente paciente;
+
+    private LocalDateTime data;
+}
---
+++ src/main/java/com/vollmed/api/domain/consulta/DadosDetalhamentoConsulta.java
@@ -0,0 +1,6 @@
+package com.vollmed.api.domain.consulta;
+
+public class DadosDetalhamentoConsulta {
+    public DadosDetalhamentoConsulta(Object o, Object o1, Object o2, Object o3) {
+    }
+}
