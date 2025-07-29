95bbeec52ffa2b25b48adf46e73bfac5ee9d8366
gqroga <gqueirogat@gmail.com>
2025-07-29 10:51:35 -0300

    feat(security): Finaliza implementação de autenticação JWT e filtros

--- src/main/java/com/vollmed/api/infra/security/TokenService.java
+++ src/main/java/com/vollmed/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package com.vollmed.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.vollmed.api.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -19,7 +20,7 @@ public class TokenService {
 
     public String gerarToken(Usuario usuario) {
         try {
-            var algoritmo = Algorithm.HMAC256("secret");
+            var algoritmo = Algorithm.HMAC256(secret);
             return JWT.create()
                     .withIssuer("API Voll.med")
                     .withSubject(usuario.getLogin())
@@ -27,6 +28,19 @@ public class TokenService {
                     .sign(algoritmo);
         } catch (JWTCreationException exception) {
             throw new RuntimeException("Erro ao gerrar token JWT", exception);
+        }
+    }
+
+    public String getSubject (String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API Voll.med")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token JWT inválido ou expirado!");
         }
     }
 
