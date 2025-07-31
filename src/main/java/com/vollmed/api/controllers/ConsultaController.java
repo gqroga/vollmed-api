2e93add6a69c61f88674dd3589f419f663b98a6c
gqroga <gqueirogat@gmail.com>
2025-07-31 15:57:23 -0300

    feat(docs): Adiciona configuração do Springdoc OpenAPI para docuemntação da API

--- src/main/java/com/vollmed/api/controllers/ConsultaController.java
+++ src/main/java/com/vollmed/api/controllers/ConsultaController.java
@@ -3,6 +3,7 @@ package com.vollmed.api.controllers;
 import com.vollmed.api.domain.consulta.AgendaDeConsultas;
 import com.vollmed.api.domain.consulta.DadosAgendamentoConsulta;
 import com.vollmed.api.domain.consulta.DadosCancelamentoConsulta;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("consultas")
+@SecurityRequirement(name = "bearer-key")
 public class ConsultaController {
 
     @Autowired
--- src/main/java/com/vollmed/api/controllers/PacienteController.java
+++ src/main/java/com/vollmed/api/controllers/PacienteController.java
@@ -1,6 +1,7 @@
 package com.vollmed.api.controllers;
 
 import com.vollmed.api.domain.paciente.*;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("pacientes")
+@SecurityRequirement(name = "bearer-key")
 public class PacienteController {
 
     @Autowired
